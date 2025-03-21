be6ca2aaab6e9345b018639c1c28fdd4435465d3
manalmarsi <marsimanal2001@gmail.com>
2025-03-21 09:41:25 -0700

    Update models.py

--- myprj/myapp/models.py
+++ myprj/myapp/models.py
@@ -9,13 +9,9 @@ class Employe(models.Model):
     last_name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     email = models.EmailField()
-<<<<<<< HEAD
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-=======
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
->>>>>>> f40ded6063f4fde36eafc1953ad440b90930e5e9
