6b1d527deaad186526159135c4b2e8088ac9f163
manalmarsi <marsimanal2001@gmail.com>
2025-03-21 07:08:09 -0700

    Update models.py

--- myprj/myapp/models.py
+++ myprj/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
